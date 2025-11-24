411bf7650f315a69320035727ec1c7ce26941ba8
avalanche557 <abhinavkarn55@gmail.com>
2025-11-24 23:50:08 +0530

    useDebounce hooks

---
+++ useDebounce.js
@@ -0,0 +1,14 @@
+import { useEffect } from "react"
+
+export const useDebounce = (value, delay = 500) => {
+    const [debouncedValue, setDebouncedValue] = useState(value)
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setDebouncedValue(value)
+        }, delay)
+        return () => clearTimeout(timeout)
+    }, [value, delay])
+
+    return debouncedValue
+}
